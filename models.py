6876478460e9bd35540bce9681c10dc75e895d00
4mh23cs158-pixel <4mh23cs158@gmail.com>
2026-02-19 15:30:19 +0530

    push

--- models.py
+++ models.py
@@ -5,5 +5,7 @@ from datetime import datetime
 class User(Base):
     __tablename__="users"
     id=Column(Integer,primary_key=True,index=True)
+    user_name=Column(String,index=True)
     email=Column(String,index=True)
     password=Column(String)
+    API_KEY=Column(String)
--- schemas/User_schema.py
+++ schemas/User_schema.py
@@ -3,4 +3,7 @@ from pydantic import BaseModel
 class UserSchema(BaseModel):
     email:str
     password:str
+
+class UserUpdateAPI_KEY(BaseModel):
+    API_KEY:str
     
